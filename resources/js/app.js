dad2566acf70f70415d0d636a067567c5330637c
Mark Halcroft <dangermark@gmail.com>
2021-07-23 10:11:29 +1000

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,17 @@
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+import { createApp} from "vue";
+
+import AppLayout from "./layouts/AppLayout";
+
+createApp({
+    components: {
+        AppLayout
+    }
+}).mount('#app')
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+    purge: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                cyan: colors.cyan,
+            }
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms')],
+};
